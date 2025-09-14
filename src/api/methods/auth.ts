906ef88e7349deeac1f6b86b3b9392f6de1e83ca
Rowan <1962408497@qq.com>
2025-09-14 14:15:01 +0800

    :heavy_plus_sign: feat: 添加 vue-use

--- src/api/methods/auth.ts
+++ src/api/methods/auth.ts
@@ -1,5 +1,6 @@
 import {alovaInstance} from "@/api";
+import type {RequestBody} from "alova";
 
-export const loginApi = <T>(data) => {
+export const loginApi = <T>(data: RequestBody) => {
   return alovaInstance.Post<T>('/api/auth/login', data)
 }
